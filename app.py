06c6ed60d2def12006a4521659abeb992e78b8ba
c0debrain <codebrain@gmail.com>
2018-11-04 12:11:59 +1100

    update template

--- app.py
+++ app.py
@@ -1,24 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+chatterbot = ChatBot("codey")
+#chatterbot = ChatBot("codey", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+#chatterbot.set_trainer(ChatterBotCorpusTrainer)
+#english_bot.train("chatterbot.corpus.english")
+#english_bot.train("chatterbot.corpus.english")
 
-english_bot.set_trainer(ChatterBotCorpusTrainer)
-english_bot.train("chatterbot.corpus.english")
+# chatterbot.train(
+#     "chatterbot.corpus.english.greetings",
+#     "chatterbot.corpus.english.conversations"
+# )
 
+def train():
+    chatterbot = ChatBot("codey", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+    chatterbot.set_trainer(ChatterBotCorpusTrainer)
+    return chatterbot.train("./corpus")
+
+train()
 
 @app.route("/")
+@app.route("/index")
 def home():
     return render_template("index.html")
+
+@app.route("/train")
+def get_train():
+    return render_template("train.html")
 
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    return str(chatterbot.get_response(userText))
 
+@app.route("/train-start")
+def get_train_bot():
+    train()
+    return redirect(url_for('index'))
+    #return str("<h1>Trained! go <a href='/'>here</a> to talk to me.</h1>")
 
 if __name__ == "__main__":
     app.run()
