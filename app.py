6e33afe475212e931e474362ab1d8a50f01e12e3
c0debrain <codebrain@gmail.com>
2018-11-04 15:39:02 +1100

    add upload support

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.storage import SQLStorageAdapter
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['UPLOAD_FOLDER'] = "./corpus/"
 
 def delfile(filename):
     if os.path.exists(filename):
@@ -51,6 +53,21 @@ def get_train_bot():
     clearEntry(chatterbot.storage)
     train()
     return redirect(url_for('home'))
+
+@app.route('/service.yml')
+def return_files_tut():
+	try:
+		return send_file('./corpus/service.yml', attachment_filename='service.yml')
+	except Exception as e:
+		return str(e)
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+      return get_train_bot()
+
 
 if __name__ == "__main__":
     app.run()
